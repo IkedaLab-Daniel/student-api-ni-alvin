fa4b13b273a5bc9723ae07772f7159425903dd96
Alvin Castro <castroalvinjr64@gmail.com>
2025-11-08 10:35:16 +0800

    Cannot connect with post: Returning 404 error

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Activity;
+use Illuminate\Http\Request;
+
+
+class ActivityController extends Controller
+{
+    // list all the activities
+    public function index(){
+        return response()->json(Activity::all(), 200);
+    }
+
+    // create activity
+    public function store(Request $request){
+        $request->validate([
+            "title" => "required",
+            "description" => "required",
+            "status" => "required|in:pending, completed"
+        ]);
+
+        $activity = Activity::create($request->all());
+
+        return response()->json(Activity::all(), 201);
+
+        
+    }
+
+    // view single activity
+     public function show($id){
+        $activity = Activity::find($id);
+
+        if(!$activity){
+            return response()->json(["message" => "Activity not found"], 404);
+        }
+
+        return response()->json($activity, 200);
+    }
+
+    // update activity
+    public function update(Request $request, $id){
+        $activity = Activity::find($id);
+
+         if(!$activity){
+            return response()->json(["message" => "Activity not found"], 404);
+        }
+
+        $activity->update($request->all());
+
+        return response()->json($activity, 200);
+    }
+    // delete activity
+     public function delete($id){
+        $activity = Activity::find($id);
+
+         if(!$activity){
+            return response()->json(["message" => "Activity not found"], 404);
+        }
+
+        $activity->delete($request->all());
+
+        return response()->json($activity, 200);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityController;
+
+Route::get('/activities', [ActivityController::class, 'index']);
+Route::post('/activities', [ActivityController::class, 'store']);
+Route::get('/activities/{id}', [ActivityController::class, 'show']);
+Route::put('/activities/{id}', [ActivityController::class, 'update']);
+Route::delete('/activities/{id}', [ActivityController::class, 'delete']);
